5b38116f2a7b739fd514a689edab556e3ddf33eb
Guilherme <guiilopes666@gmail.com>
2026-08-14 22:19:41 -0300

    subindo arquivos

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-// funcao que valida se os inputs não estão vazios
-export function validarCampos(nome, email){
-    if(nome.trim() === "" || email.trim()===""){
-        return false;
-    }
-    return true;
-}
